3f471ad872f486c4074ac3663ee9736355de37db
Ivan Ramirez <ivan@astrochasqui.com>
2017-08-30 10:25:11 -0700

    first commit

---
+++ code.py
@@ -0,0 +1,7 @@
+def collatz(number):
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = 3 * number + 1
+        print(number)
